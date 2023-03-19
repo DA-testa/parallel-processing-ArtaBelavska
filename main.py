f86c3ac72a4a31ffc02c3dc4995f2404d48aff26
ArtaBelavska <124885266+ArtaBelavska@users.noreply.github.com>
2023-03-19 22:47:32 +0000

    tests1

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 # python3
+import time
 
 def parallel_processing(n, m, data):
     output = []
+    apstradesLaiks=[(l, i) for i, l in s(data)]
+    pavediens= [0]*n
+    #pavediens=[(i, 0) for i in range(n)]
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
-
+while apstradesLaiks:
+    skaits= pavediens.skaits(min(pavediens))
+    output.append((skaits, pavediens[skaits]))
+    ilgums, apstrade= apstradesLaiks.pop(0)
+    pavediens[skaits]+= ilgums
+    #output.append((pavediens[0], pavediens[1]))
     return output
 
 def main():
@@ -13,19 +22,16 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
-
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    n, m=map(int, input().split())
 
     # TODO: create the function
+    data=list(map(int, input().split()))
     result = parallel_processing(n,m,data)
-    
+    for pavediens, time in result:
+        print(pavediens, time)
     # TODO: print out the results, each pair in it's own line
-
-
 
 if __name__ == "__main__":
     main()
